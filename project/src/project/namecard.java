a517a5c8b6a05577a56bb37bbdc1e90f8ef54751
YeongJUnJoH171 <44230805+YeongJUnJoH171@users.noreply.github.com>
2018-11-28 17:06:19 +0900

    파일 입출력 기능 추가

--- project/src/project/namecard.java
+++ project/src/project/namecard.java
@@ -2,10 +2,40 @@ package project;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 public class namecard extends JFrame{
 private JTextField tf = new JTextField(100);
 private JTextArea ta = new JTextArea(40, 100);
+final JFileChooser SaveAs = new JFileChooser();
+public void saveAs() {
+	 final JFileChooser SaveAs = new JFileChooser();
+     SaveAs.setApproveButtonText("Save");
+     int actionDialog = SaveAs.showOpenDialog(this);
+     if (actionDialog != JFileChooser.APPROVE_OPTION) {
+        return;
+     }
 
+     File fileName = new File(SaveAs.getSelectedFile() + ".txt");
+     BufferedWriter outFile = null;
+     try {
+        outFile = new BufferedWriter(new FileWriter(fileName));
+
+        ta.write(outFile);   // *** here: ***
+
+     } catch (IOException ex) {
+        ex.printStackTrace();
+     } finally {
+        if (outFile != null) {
+           try {
+              outFile.close();
+           } catch (IOException e) {
+              // one of the few times that I think that it's OK
+              // to leave this blank
+           }
+        }
+     }
+  
+}
 public namecard() {
 	setTitle("고려대학교 관현악단 명단관리");
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -14,6 +44,13 @@ public namecard() {
 	c.add(new JLabel("이름/학번/학과/악기/전화번호/출신 중학교/출신 고등학교/간단한 각오 순으로 적고 느낌표(!)를 적은 뒤 <Enter> 키를 입력하세요"));
 	c.add(tf);
 	c.add(new JScrollPane(ta));
+	c.add(new JPanel(){{add(new JButton(new AbstractAction("Save As") {
+
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+           saveAs();
+        }
+     }));}}, BorderLayout.SOUTH);
 	
 	tf.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
